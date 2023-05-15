4af9aff60636dc60c7544436480f4822fcf31b7c
saimsaleem <engssaleem13@gmail.com>
2023-05-15 19:29:43 +0500

    A3

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,7 +8,7 @@ user.use(bodyParser.json());
 user.use(cors( { origin: '*' , } ));
 user.use(express.urlencoded({extended: false}));
 
-user.post('/addTransport',controller.addTransport);
+user.post('/addTransport',controller.addTransport); 
 user.get('/getTransport',controller.getTransport);
 // user.delete('/deleteTransport/:id',controller.deleteTransport);
 // user.get('/findTransport/:namee',controller.findTransport);
